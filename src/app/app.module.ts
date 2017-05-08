5f4bd0e6838f231161f8980999b3ce163743aaed
llighter <hadbsgk@gmail.com>
2017-05-09 01:40:01 +0900

    connect to firebase and add C,R

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -10,6 +13,15 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const config = {
+    apiKey: "AIzaSyBoY4qMLO_ImFv3qXtKD96i1QbY3a34mpI",
+    authDomain: "studycard-17976.firebaseapp.com",
+    databaseURL: "https://studycard-17976.firebaseio.com",
+    projectId: "studycard-17976",
+    storageBucket: "studycard-17976.appspot.com",
+    messagingSenderId: "49727878941"
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +33,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  categories: FirebaseListObservable<any[]>;
+  userName: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private afAuth: AngularFireAuth, private db: AngularFireDatabase) {
+    this.user = afAuth.authState;
+    this.items = db.list('items');
 
+    this.user.subscribe((user: firebase.User) => {
+      if(user != null) {
+        this.userName = user.displayName;
+        this.categories = this.db.list(user.uid);
+        this.categories.subscribe(categories => console.log(`[Categories]${categories}`));
+        console.log(`[constructor]userName : ${this.userName}`);
+      } else {
+        this.userName = 'Not yet Logged in..';
+      }
+    });
+  }
+
+  login(): void {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout(): void {
+     this.afAuth.auth.signOut();
+  }
+
+  addCategory(category: string): void {
+    this.categories.push(category);
   }
 
 }
