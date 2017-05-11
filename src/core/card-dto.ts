5c101e1c548fe0e10f2574bc57cd1843c24cf6ab
llighter <hadbsgk@gmail.com>
2017-05-11 12:47:41 +0900

    add DTO pattern

---
+++ src/core/card-dto.ts
@@ -0,0 +1,10 @@
+export class CardDTO {
+    question: string;
+    answer: string;
+    source: string;
+    failCount: number;
+
+    // TODO: add additional member
+    // resource: string;
+    // tag: string[];
+}
