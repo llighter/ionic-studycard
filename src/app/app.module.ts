fa165d90fe9f6b63ca6ac0051c86a3ef5344b355
llighter <hadbsgk@gmail.com>
2017-05-12 17:25:03 +0900

    move firebase config from app.module to environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,10 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { CardDetail } from '../pages/card-detail/card-detail';
+import { environment } from '../environments/environment';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-export const config = {
-    apiKey: "AIzaSyBoY4qMLO_ImFv3qXtKD96i1QbY3a34mpI",
-    authDomain: "studycard-17976.firebaseapp.com",
-    databaseURL: "https://studycard-17976.firebaseio.com",
-    projectId: "studycard-17976",
-    storageBucket: "studycard-17976.appspot.com",
-    messagingSenderId: "49727878941"
-};
 
 @NgModule({
   declarations: [
@@ -36,7 +28,7 @@ export const config = {
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(config),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
   ],
