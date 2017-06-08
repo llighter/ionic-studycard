a09d46b03e5ab5aa6d7da2f3ddc618b76875ced8
llighter <hadbsgk@gmail.com>
2017-06-08 13:11:32 +0900

    code review

--- src/pages/card-detail/card-detail.ts
+++ src/pages/card-detail/card-detail.ts
@@ -5,16 +5,10 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 
 import { CardDTO } from '../../core/card-dto';
 import { Observable } from 'rxjs/Observable';
-// import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import * as firebase from 'firebase/app';
 
-/**
- * Generated class for the CardDetail page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @IonicPage()
 @Component({
   selector: 'page-card-detail',
@@ -26,6 +20,7 @@ export class CardDetail implements OnInit{
   uid: string;
   show: boolean = true;
   stageCount: number[] = [0,0,0,0,0];
+  currentSegment: string;
 
   card: CardDTO;
   queryObservable: FirebaseListObservable<any[]>;
@@ -42,6 +37,7 @@ export class CardDetail implements OnInit{
     console.log(`[card-detail]${JSON.stringify(this.navParams)}`);
     this.categoryTitle = this.navParams.get('categoryName');
 
+    this.currentSegment = "stage1";
     this.card = new CardDTO();
     this.stageSubject = new BehaviorSubject(1);
 
@@ -58,44 +54,24 @@ export class CardDetail implements OnInit{
           }
         });
 
-        // TODO: This is the Best way? Come on man...
-        // option : use function
-        this.db.list(`${this.uid}/${this.categoryTitle}`, {
-          query: {
-            orderByChild: 'stage',
-            equalTo: 1,
-          }
-        }).subscribe(list => this.stageCount[0] = list.length);
-        this.db.list(`${this.uid}/${this.categoryTitle}`, {
-          query: {
-            orderByChild: 'stage',
-            equalTo: 2,
-          }
-        }).subscribe(list => this.stageCount[1] = list.length);
-        this.db.list(`${this.uid}/${this.categoryTitle}`, {
-          query: {
-            orderByChild: 'stage',
-            equalTo: 3,
-          }
-        }).subscribe(list => this.stageCount[2] = list.length);
-        this.db.list(`${this.uid}/${this.categoryTitle}`, {
-          query: {
-            orderByChild: 'stage',
-            equalTo: 4,
-          }
-        }).subscribe(list => this.stageCount[3] = list.length);
-        this.db.list(`${this.uid}/${this.categoryTitle}`, {
-          query: {
-            orderByChild: 'stage',
-            equalTo: 5,
-          }
-        }).subscribe(list => this.stageCount[4] = list.length);
+        this.updateStageCount();
       }
     });
   }
 
+  updateStageCount(): void {
+    this.stageCount.forEach((stage, index) => {
+      this.db.list(`${this.uid}/${this.categoryTitle}`, {
+        query: {
+          orderByChild: 'stage',
+          equalTo: index+1,
+        }
+      }).subscribe(list => this.stageCount[index] = list.length);
+    });
+  }
+
   insert(data:any) {
-    if(!this.isStageFull()) {
+    if(!this.isStageFull(1)) {
       this.card.question = data.question;
       this.card.answer = data.answer;
       this.card.source = data.source;
@@ -105,6 +81,12 @@ export class CardDetail implements OnInit{
       this.db.list(`${this.uid}/${this.categoryTitle}`).push(this.card);
     } else {
       console.log(`[x]Can't insert card into stage1.. It is now Full...`);
+      let alert = this.alertCtrl.create({
+        title: 'First Stage is now Full!',
+        subTitle: `Can't insert card into stage1.. It is now Full...`,
+        buttons: ['OK']
+      });
+      alert.present();
     }
   }
 
@@ -148,56 +130,77 @@ export class CardDetail implements OnInit{
   success(key:string, card: CardDTO) {
 
     if(card.stage >= 1 && card.stage < 5) {
-      card.stage++;
-      card.failCount = 0;
-      console.log(`[success-stage]${card.stage}`);
-      this.queryObservable.push(card);
-    } 
-
-    this.queryObservable.remove(key);
-    
+      if(!this.isStageFull(++card.stage)) {
+        card.failCount = 0;
+        this.queryObservable.push(card);
+        this.queryObservable.remove(key);
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Second Stage is now Full!',
+          subTitle: `Can't insert card into stage2.. It is now Full...`,
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+      
+    }
     // TODO: (개선)카드 앞면으로 초기화
     this.show = true;
   }
 
   fail(key:string, card: CardDTO) {
 
-    card.stage = 1;
     card.failCount++;
 
-  
     // TODO: 1단계 스테이지가 가득 찾는지 확인해야한다.
-    if(!this.isStageFull) {
-      alert("[x]1st stage is full...");
+    if(this.isStageFull(1) && card.stage != 1) {
+      let alert = this.alertCtrl.create({
+        title: 'First Stage is now Full!',
+        subTitle: `Can't move card into stage1.. Need to Clear your stage1...`,
+        buttons: ['OK']
+      });
+      alert.present();
+    } else {
+      card.stage = 1;
+      this.queryObservable.remove(key);
+      this.queryObservable.push(card);
     }
-
-    this.queryObservable.remove(key);
-    this.queryObservable.push(card);
 
     // TODO: (개선)카드 앞면으로 초기화
     this.show = true;
   }
 
   drop(key:string, card: CardDTO) {
-
     console.log(`[drop-card]${key}`);
-
     this.queryObservable.remove(key);
   }
 
   filterBy(stage: string) {
-    // How can I know the value inside subject ?
-    // this.stageSubject.next(stage);
     this.stageSubject.next(stage);
-
-    console.log(`[Stage]${this.stageSubject.getValue()}`);
+    console.log(`[Current-Stage]${this.stageSubject.getValue()}`);
   }
   
-  isStageFull(): boolean {
-    let isFull: boolean;
+  isStageFull(level): boolean {
+    let isFull: boolean = false;
     console.log(`[stage-count]${this.stageCount}`);
 
-    isFull = this.stageCount[0] == 30 ? true : false;
+    switch(level) {
+      case 1:
+        isFull = this.stageCount[0] == 30 ? true : false;
+        break;
+      case 2:
+        isFull = this.stageCount[1] == 30*2 ? true : false;
+        break;
+      case 3:
+        isFull = this.stageCount[2] == 30*5 ? true : false;
+        break;
+      case 4:
+        isFull = this.stageCount[3] == 30*8 ? true : false;
+        break;
+      case 5:
+        isFull = this.stageCount[4] == 30*15 ? true : false;
+        break;
+    }
 
     return isFull;
   }
